a5bf84a6f584e0fd49783cc2de3f54a2d5fc3f19
MalkareddyJithender <jithendermalkareddy@gmail.com>
2020-08-24 17:52:55 +0530

    setup production webpack build

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Git Imp Commands 
+
+git init -- create a new git repository(where our directory lives)
+
+git status -- view the changes to our project code
+
+git add -- add files to staged changes area
+
+git commit -- Creates a new commit(savepoint) with files from staged changes area
+
+git log -- print git commits
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,55 @@
 // entry ->output
 const path = require('path')
+const extractTextPlugin = require('extract-text-webpack-plugin');
+const CSSExtract = new extractTextPlugin('styles.css');
 
-module.exports = 
-{ 
-    entry:'./src/app.js',
-    output:
-    {
-        path:path.join(__dirname,'public'),
-        filename:'bundle.js'
-    },
-    module:
-    {
-        rules:[{
-            loader:'babel-loader',
-            test:/\.js$/,
-            exclude:/node_modules/
-        },{
-            test: /\.s?css$/,
-            use:[
-                //push js representation css along with style tag in to dom
-                'style-loader',
-                //it should load-in .css file and collect css then converts to js rep of css
-                'css-loader',
-                //it should grab .scss file and use node-sass behind the scenes to convert/compile/transform to .css file. 
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool:'eval-cheap-module-source-map',
-    devServer:
-    {  
-        contentBase:path.join(__dirname,'public'),
-         historyApiFallback:true
-    }
+module.exports = (env) =>
+{
+    const isProduction = env === 'production';
+    
+    return { 
+        entry:'./src/app.js',
+        output:
+        {
+            path:path.join(__dirname,'public'),
+            filename:'bundle.js'
+        },
+        module:
+        {
+            rules:[{
+                loader:'babel-loader',
+                test:/\.js$/,
+                exclude:/node_modules/
+            },{
+                test: /\.s?css$/,
+                use:CSSExtract.extract({
+                    use:[
+                        //it should load-in .css file and collect css then converts to js rep of css
+                        {
+                            loader:'css-loader',
+                            options:{
+                                sourceMap:true
+                            }
+                        },
+                        //it should grab .scss file and use node-sass behind the scenes to convert/compile/transform to .css file. 
+                        {
+                            loader:'sass-loader',
+                            options:{
+                                sourceMap:true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins:[
+            CSSExtract
+        ],
+        devtool:isProduction ? 'source-map' :'inline-source-map',
+        devServer:
+        {  
+            contentBase:path.join(__dirname,'public'),
+            historyApiFallback:true
+        }
+    };
 };
