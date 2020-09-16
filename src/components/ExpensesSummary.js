5c7fd3bb962a5bf3578a862edc95739ba74f8edc
MalkareddyJithender <jithendermalkareddy@gmail.com>
2020-09-16 16:34:29 +0530

    updated expensesSummary component

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -12,6 +12,7 @@ export class ExpensesSummary extends React.Component
     render()
     {
         const expenseWord = this.props.expensesCount === 1 ? 'expense' : 'expenses';
+        const expenseWord2 = this.props.hiddenExpensesCount <= 1 ? 'expense' : 'expenses';
         return (
             <div className="page-header">
                 <div className="content-container">
@@ -20,6 +21,17 @@ export class ExpensesSummary extends React.Component
                  viewing <span>{this.props.expensesCount}</span> {expenseWord} totalling  <span>{ currencyFormatter.format(this.props.expensesTotal/100,{code:'INR'})}</span> 
                 </h1> 
                 }
+                {
+                    this.props.hiddenExpensesCount === 0 ?
+                    <h3 className="page-header__subtitle">
+                        <span>{this.props.hiddenExpensesCount}</span> {expenseWord2} hidden.
+                    </h3>
+                    :
+                    <h3 className="page-header__subtitle">
+                        <span>{this.props.hiddenExpensesCount}</span> {expenseWord2} hidden because of default filters.
+                    </h3>              
+                }
+
                 <div className="page-header__actions">
                 <Link className="button" to="/create">Add Expense</Link>
                 </div>
@@ -34,7 +46,8 @@ const mapStateToProps = (state) =>
     const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
     return {
         expensesCount:visibleExpenses.length,
-        expensesTotal:getExpensesTotal(visibleExpenses)
+        expensesTotal:getExpensesTotal(visibleExpenses),
+        hiddenExpensesCount:state.expenses.length - visibleExpenses.length
     }
 };
 
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -4,12 +4,12 @@ import {ExpensesSummary} from '../../components/ExpensesSummary';
 
 test('should render Expenses Summary component correctly with 1 expense',() =>
 {
-    const result = shallow(<ExpensesSummary expensesCount={1} expensesTotal={1000000} />);
+    const result = shallow(<ExpensesSummary expensesCount={1} expensesTotal={1000000} hiddenExpensesCount={0} />);
     expect(result).toMatchSnapshot();
 });
 
 test('should render Expenses Summary component correctly with multiple expenses',() =>
 {
-    const result = shallow(<ExpensesSummary expensesCount={5} expensesTotal={23445000} />);
+    const result = shallow(<ExpensesSummary expensesCount={5} expensesTotal={23445000} hiddenExpenses={2} />);
     expect(result).toMatchSnapshot();
 });
